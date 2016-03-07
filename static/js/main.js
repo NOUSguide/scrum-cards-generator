5cfc3c0a729897be53b00c5fe6ceea2c7a5d8dae
Matthias Schoetta <m.schoetta@nousguide.com>
2016-03-07 20:27:05 +0100

    added qrcodes

--- static/js/main.js
+++ static/js/main.js
@@ -25,6 +25,13 @@ function generateCards() {
 
     prepareTemplateData(url, ids).then(function(data) {
         $('#cards').html( template(data) );
+        data.issues.forEach(function(entry) {
+            entry.forEach(function(issue) {
+                if (issue.id) {
+                    makeCode(issue.id);
+                }
+            });
+        });
     });
 }
 
@@ -38,28 +45,20 @@ function init() {
         }
     });
     setTimeout(function() { $('#ids').focus(); }, 1000);
-
-    // TODO: remove
-    // generateCards();
 }
 
 function prepareTemplateData(url, ids) {
     var api = new RedmineAPI({ url: url });
-
     var issuesPromises = ids.map(function(id) {
         return api.getIssueById(id);
     } );
-
     var cardsCountPerPage = 12;
     if (issuesPromises.length % cardsCountPerPage != 0) {
-
         var count = (issuesPromises.length % cardsCountPerPage - cardsCountPerPage) * -1;
-        console.log('EmptyCount %s', count);
         for (var i = 0; i < count; i++) {
             issuesPromises.push(new Issue());
         }
     }
-
     return Promise.all(issuesPromises).then(function(issues) {
         return { issues: splitArray(issues,3) };
     }).catch(console.error);
@@ -67,18 +66,28 @@ function prepareTemplateData(url, ids) {
 
 function splitArray(array, step) {
     var newArray = [];
-
     for (var i = 0; i < array.length; i+= step) {
         var newSubArray = [];
-
         for (var j = i; j < i+step; j++) {
             if (j>array.length) break;
-
             newSubArray.push(array[j]);
         };
-
         newArray.push(newSubArray);
     }
-
     return newArray;
+}
+
+function makeCode(id) {
+    var qrCodeId = 'qrcode'+id;
+    console.log('qrCodeId %s', qrCodeId);
+    var link = "https://redmine.nousguide.com/issues/" + id;
+    var qrcode = new QRCode(document.getElementById(qrCodeId), {
+        text: link,
+        width: 60,
+        height: 60,
+        colorDark : "#000000",
+        colorLight : "#ffffff",
+        correctLevel : QRCode.CorrectLevel.H
+    });
+    qrcode.makeCode(link);
 }
